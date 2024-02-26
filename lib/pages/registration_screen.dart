0e3e88fd9cf511d8b45fb2f81f35d9a32b3a519d
Ibrahim Khan <aitvetibrahim@gmail.com>
2024-02-26 12:50:34 +0600

    Register Screen Ui done

---
+++ lib/pages/registration_screen.dart
@@ -0,0 +1,251 @@
+import 'package:e_medicine/theme.dart';
+import 'package:e_medicine/widget/general_logo_space.dart';
+import 'package:flutter/material.dart';
+
+import '../widget/button_primary.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  bool _secureText = true;
+  showHide() {
+    setState(() {
+      _secureText = !_secureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          const GeneralLogoSpace(),
+          Container(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'register'.toUpperCase(),
+                  style: regularTextStyle.copyWith(
+                    fontSize: 25,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'Register new your account',
+                  style: regularTextStyle.copyWith(
+                    fontSize: 15,
+                    color: greyLightColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0x04000000),
+                        offset: Offset(0, 1),
+                        blurRadius: 4,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                    color: whiteColor,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Full Name',
+                      hintStyle: lightTextStyle.copyWith(
+                        fontSize: 15,
+                        color: greyLightColor,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0x04000000),
+                        offset: Offset(0, 1),
+                        blurRadius: 4,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                    color: whiteColor,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Email Address',
+                      hintStyle: lightTextStyle.copyWith(
+                        fontSize: 15,
+                        color: greyLightColor,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0x04000000),
+                        offset: Offset(0, 1),
+                        blurRadius: 4,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                    color: whiteColor,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Phone',
+                      hintStyle: lightTextStyle.copyWith(
+                        fontSize: 15,
+                        color: greyLightColor,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0x04000000),
+                        offset: Offset(0, 1),
+                        blurRadius: 4,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                    color: whiteColor,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Home Address',
+                      hintStyle: lightTextStyle.copyWith(
+                        fontSize: 15,
+                        color: greyLightColor,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0x04000000),
+                        offset: Offset(0, 1),
+                        blurRadius: 4,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                    color: whiteColor,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextField(
+                    obscureText: _secureText,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: showHide,
+                        icon: _secureText
+                            ? const Icon(
+                                Icons.visibility_off,
+                                size: 20,
+                              )
+                            : const Icon(
+                                Icons.visibility,
+                                size: 20,
+                              ),
+                      ),
+                      border: InputBorder.none,
+                      hintText: 'Password',
+                      hintStyle: lightTextStyle.copyWith(
+                        fontSize: 15,
+                        color: greyLightColor,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: ButtonPrimary(
+                    onTap: () {},
+                    text: 'register',
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account? ',
+                      style: lightTextStyle.copyWith(
+                        color: greyLightColor,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      'Login now',
+                      style: boldTextStyle.copyWith(
+                        color: greenColor,
+                        fontSize: 15,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
